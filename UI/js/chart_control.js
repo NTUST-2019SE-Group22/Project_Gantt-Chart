f2f95ff7695c6166c78f106687f09161bb83ebb9
DannyLeee <dannylee94049@gmail.com>
2019-12-21 21:26:35 +0800

    [Update] js merging (sync_and_validation)

--- UI/js/chart_control.js
+++ UI/js/chart_control.js
@@ -52,6 +52,20 @@ $(document).ready(function () {
     });
     /** ----- */
 
+    /**
+     * Validation
+     * Dynamic validate the form table, if valid then sow chart
+     */
+    var form = document.getElementById('data-table');
+    form.addEventListener('change', function(event) {
+        if (form.checkValidity() === false)
+            event.preventDefault();
+        // SYNC
+        else 
+            show();
+        form.classList.add('was-validated');
+    }, false);
+
 });
 
 /**
@@ -298,3 +312,76 @@ function startRead() {
 }
 /** ----- */
 
+/**
+ * Synchronization
+ * Add listener on date input to synchronize the end day and duration
+ */
+
+// paddingLeft
+// padding '0' before the `str` until `length`
+// Used in date padding zero
+function paddingLeft(str, length){
+    str = String(str);  
+    // console.log(str.length);
+    if(str.length >= length)
+        return str;
+    else
+        return paddingLeft("0" +str,length);
+}
+
+// Start_End_Listener
+// Get what row change start or end day then sync the duration
+function Start_End_Listener() {
+    var tableRows = $("#task_table > tbody > tr").length;
+    var taskID;
+
+    for(var i = 1; i <= tableRows; i++) {
+        var temp = '#task_table > tbody > tr:nth-child(' + i + ')';
+        taskID = $(temp).attr("id");
+        if (taskID == $(':focus').get(0).parentElement.parentElement.parentElement.id) {
+            taskID = '#' + taskID;
+            break;
+        }
+    }
+    // console.log(taskID);
+    var startDay = $(taskID).find('input[name="startDay"]').val();
+    var endDay = $(taskID).find('input[name="endDay"]').val();
+
+    // type convertion
+    startDay = new Date(startDay);
+    endDay = new Date(endDay);
+    var duration = (endDay - startDay) / 86400000;
+    $(taskID).find('input[name="duration"]').val(duration);
+}
+
+// Duration_Listener
+// Get what row change duration then sync the end day
+function Duration_Listener()
+{
+    var tableRows = $("#task_table > tbody > tr").length;
+    var taskID;
+
+    for(var i = 1; i <= tableRows; i++) {
+        var temp = '#task_table > tbody > tr:nth-child(' + i + ')';
+        taskID = $(temp).attr("id");
+        if (taskID == $(':focus').get(0).parentElement.parentElement.parentElement.id) {
+            taskID = '#' + taskID;
+            break;
+        }
+    }
+
+    var startDay = $(taskID).find('input[name="startDay"]').val();
+    var duration = $(taskID).find('input[name="duration"]').val();
+
+    // type convertion
+    startDay = new Date(startDay);
+    duration *= 86400000;
+    var endDay = new Date(startDay.getTime() + duration);
+    var year = endDay.getUTCFullYear();
+    var month = endDay.getUTCMonth() + 1;
+    var date = endDay.getUTCDate();
+    endDay = year + '-' + paddingLeft(month, 2) + '-' + paddingLeft(date, 2);
+    // console.log(endDay);
+    $(taskID).find('input[name="endDay"]').val(endDay);
+    // console.log('taskID: ' + taskID + ' d');
+}
