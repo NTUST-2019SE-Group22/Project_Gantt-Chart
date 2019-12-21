faa5ecc579ce20010c5418e04aea6da4800ca1c3
DannyLeee <dannylee94049@gmail.com>
2019-12-21 21:02:12 +0800

    [Update] js merging (drawChart)

--- UI/js/chart_control.js
+++ UI/js/chart_control.js
@@ -111,3 +111,118 @@ function imageOutput()
     $("#downloadSVG").attr("href", url); 
 }
 /** ----- */
+
+/**
+ * Draw chart
+ * Draw the chart on the page from the form table
+ */
+
+// Load google API 
+google.charts.load('current', {
+    'packages': ['gantt']
+});
+google.charts.setOnLoadCallback(initialChart);
+
+// daysToMilliseconds
+// Transfer the day to millisecond(API needed)
+function daysToMilliseconds(days) {
+    return days * 24 * 60 * 60 * 1000;
+}
+
+// cahrt: the container object that API draw in
+// data: the data table that store chart's tasks
+var chart;
+var data;
+
+// initialChart
+// Initial the data table columns and chart
+function initialChart() {
+    data = new google.visualization.DataTable();
+    data.addColumn('string', 'Task ID');
+    data.addColumn('string', 'Task Name');
+    data.addColumn('string', 'Resource');
+    data.addColumn('date', 'Start Date');
+    data.addColumn('date', 'End Date');
+    data.addColumn('number', 'Duration');
+    data.addColumn('number', 'Percent Complete');
+    data.addColumn('string', 'Dependencies');
+    chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+}
+
+// show
+// Loop search the form table and put into data table than draw
+function show() {
+    var i;
+    var tableRows = $("#task_table > tbody > tr").length;
+    var dataRows;
+    // console.log('tableRows ' + tableRows);
+    for (i = 1; i <= tableRows; i++) {
+        var temp = '#task_table > tbody > tr:nth-child(' + i + ')';
+        var taskID = '#' + $(temp).attr("id");
+        var taskName = $(taskID).find('input[name="taskName"]').val();
+        var resource = $(taskID).find('input[name="resource"]').val();
+        var startDay = $(taskID).find('input[name="startDay"]').val();
+        var endDay = $(taskID).find('input[name="endDay"]').val();
+        var duration = $(taskID).find('input[name="duration"]').val();
+        var complete = $(taskID).find('input[name="complete"]').val();
+
+        // type convertion
+        startDay = new Date(startDay);  // to API acceptable
+        endDay = new Date(endDay);  // to API acceptable
+        duration = Number(duration);    // to number
+        duration = daysToMilliseconds(duration);    // to API acceptable
+        complete = Number(complete);
+
+        var newTask = true;
+        dataRows = data.getNumberOfRows();
+        // console.log('dataRows ' + dataRows);
+        for (var y = 0; y < dataRows; y++) {
+            if (data.getValue(y,0) == taskID) {
+                newTask = false;
+                break;
+            }
+        }
+
+        if (newTask) {
+            // console.log("new task");
+            data.addRows([[taskID, taskName, resource, startDay, endDay, duration, complete, null]]);
+        }
+        else {
+            // console.log("edit task");
+            
+            // update all attribute
+            for (var y = 0; y < dataRows; y++) {
+                if (data.getValue(y, 0) == taskID) {
+                    data.setValue(y, 1, taskName);
+                    data.setValue(y, 2, resource);
+                    data.setValue(y, 3, startDay);
+                    data.setValue(y, 4, endDay);
+                    data.setValue(y, 5, duration);
+                    data.setValue(y, 6, complete);
+                }
+            }
+        }
+    }
+
+    var options = {
+        gantt: {
+            trackHeight: 45
+        },
+        height: 180
+    };
+
+    // dynamic calculate the showing chart height
+    if ((options.gantt.trackHeight * data.getNumberOfRows() + 50) > 180)
+        options.height = options.gantt.trackHeight * data.getNumberOfRows() + 50;
+    
+    // draw to the container
+    chart.draw(data, options);
+
+    // set the output canvas width and height
+    var svgWidth = $('svg').attr("width");
+    var svgHeight = $('svg').attr("height");
+    $("#canvas").attr({width: svgWidth, height: svgHeight});
+    $("#chart_container").addClass("bg-white box-shadow");
+}
+/** ----- */
+
